ba34ec15387427b7f02073c9d7148bee15846ac1
samWidlund <sam.wid415@gmail.com>
2026-05-04 22:58:27 +0200

    publish: initial python setup

--- publish/publishProducts.py
+++ publish/publishProducts.py
@@ -1,1 +1,21 @@
 # script to publish products at mulitple platforms at once
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+client = gspread.authorize(credentials)
+
+# Open the Google Sheet
+sheet = client.open('publishProducts').sheet1
+
+# Fetch the first row of data
+first_col = sheet.col_values(1)
+second_col = sheet.col_values(2)
+print(f"First column of data: {first_col}")
+print(f"Second column of data: {second_col}")
