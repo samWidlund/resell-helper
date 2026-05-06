8f0067aa68f4e184ea7eeb1a774caf3331026abc
samWidlund <sam.wid415@gmail.com>
2026-05-07 00:35:17 +0200

    publish: import n use pathlib for correct usage in workflow

--- publish/publishProducts.py
+++ publish/publishProducts.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 import webbrowser
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
